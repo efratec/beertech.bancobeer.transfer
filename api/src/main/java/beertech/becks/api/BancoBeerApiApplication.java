ee3f2a13d55e45ca374694b26f4d7dd856640b1a
Amanda von Zuben <amandavonzuben@gmail.com>
2020-10-10 16:46:22 -0300

    Unit Tests - TransactionService

--- api/src/main/java/beertech/becks/api/BancoBeerApiApplication.java
+++ api/src/main/java/beertech/becks/api/BancoBeerApiApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BancoBeerApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BancoBeerApiApplication.class, args);
+
 	}
 
 }
